2dbf3295d221981c4937b6bd24a0052571ae0e7f
Bryce Eadie <bryce.eadie@gmail.com>
2018-09-28 03:17:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 def prime?(number)
-  is_prime = true
+  is_prime = false
   range = 2..number-1
   array = []
   range.each do |num|
     array << num
   end
   array.each do |element|
-  if number % element == 0 || number == -1
-  is_prime = false
+  if number % element != 0 
+  is_prime = true
   end
   end
   is_prime
